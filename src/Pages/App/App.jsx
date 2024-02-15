ce9ba303b76f7bc94876008f974b80fc27218d0a
Miguel Gomez <gomezcasas.miguelangel@gmail.com>
2024-02-14 19:41:39 -0500

    Estructura inicial del proyecto

---
+++ src/Pages/App/App.jsx
@@ -0,0 +1,37 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom'
+
+import Home from '../Home'
+import Navbar from '../../Components/Navbar'
+import './App.css'
+
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: '/', element: <Home /> },
+    /* { path: '/clothes', element: <Home /> },
+    { path: '/electronics', element: <Home /> },
+    { path: '/furnitures', element: <Home /> },
+    { path: '/toys', element: <Home /> },
+    { path: '/othes', element: <Home /> },
+    { path: '/my-account', element: <MyAccount /> },
+    { path: '/my-order', element: <MyOrder /> },
+    { path: '/my-orders', element: <MyOrders /> },
+    { path: '/my-orders/last', element: <MyOrder /> },
+    { path: '/my-orders/:id', element: <MyOrder /> },
+    { path: '/sign-in', element: <SignIn /> },
+    { path: '/*', element: <NotFound /> }, */
+
+  ])
+
+  return routes
+}
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+      <Navbar />
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <div className='w-600 h-700 bg-black text-white'>
+            hola mundo de una vite de la tab a la tab a la tab a la tab a la tab
+        </div>
+    )
+}
+
+export default Home
