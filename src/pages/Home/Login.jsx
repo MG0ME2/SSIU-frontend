ac921390eb610d701612de3f72fd113430f50e1b
Miguel Gomez <gomezcasas.miguelangel@gmail.com>
2024-05-10 02:47:32 -0500

    modified signup with Alerts

--- src/pages/Home/Login.jsx
+++ src/pages/Home/Login.jsx
@@ -1,8 +1,8 @@
-import { useState, useEffect, useContext } from "react";
-import { Link, json, useNavigate } from "react-router-dom";
+import {useState, useEffect, useContext} from "react";
+import {Link, json, useNavigate} from "react-router-dom";
 import axios from 'axios';
-import { useDispatch } from 'react-redux';
-import { ToastContainer, toast } from 'react-toastify';
+import {useDispatch} from 'react-redux';
+import {ToastContainer, toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 import LayoutH from "../../components/LayoutHome/index";
@@ -12,8 +12,8 @@ import IconHomeLogin from "../../assets/Img/IconHomeLogin.svg";
 import IconEye from "../../assets/Img/IconEye.svg";
 import IconOffEye from "../../assets/Img/IconOffEye.svg";
 
-import { useLocalStorage } from '../../components/localStorage'
-import { login } from '../../redux/states/authSlice';
+import {useLocalStorage} from '../../components/localStorage'
+import {login} from '../../redux/states/authSlice';
 import ButtonPrimary from "../../components/Buttons/primary";
 
 function Login() {
@@ -22,7 +22,7 @@ function Login() {
   }
   const dispatch = useDispatch();
   let navigate = useNavigate();
-
+  
   //Variables para manejo de la contraseña
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -30,153 +30,122 @@ function Login() {
   const [emailError, setEmailError] = useState("");
   const [passwordError, setPasswordError] = useState("");
   const [alert, setAlert] = useState(null);
-
-  const togglePasswordVisibility = () => {
-    setShowPassword(!showPassword);
-  };
-
-  const validateEmail = () => {
-    if (!email.trim()) {
-      setEmailError("Debe ingresar un correo electrónico");
-      return false;
-    } else {
-      setEmailError("");
-      return true;
-    }
-  };
-
-  const validatePassword = () => {
-    if (!password.trim()) {
-      setPasswordError("Debe ingresar una contraseña");
-      return false;
-    } else {
-      setPasswordError("");
-      return true;
-    }
-  };
-
+  
   //manejo de Local Storage
   const [getToken, setToken] = useLocalStorage('token');
   const [getUser, setUser] = useLocalStorage('user');
   const [getIsLogged, setIsLogged] = useLocalStorage('isLogged');
   const [getWarnignMessage, setWarnignMessage] = useLocalStorage('warnignLogin');
-
-  const navRegisterPage = (event)=>{
-    //event.preventDefault();
-    navigate('/signup')
-  }
-
+  
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+  
   const handleLogin = async (event) => {
     event.preventDefault();
-
-    const isEmailValid = validateEmail();
-    const isPasswordValid = validatePassword();
-
-    if (isEmailValid && isPasswordValid) {
-      const formData = new FormData(event.currentTarget);
-      const form = {
-        email: formData.get("email"),
-        password: formData.get("password"),
-      };
-
-      const { data } = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/auth/login`, form);
-
-      if (data.status === parseInt("401")) {
-        setErrorMessage(data);
-      }else if(data.error){
-        // comentario de jose
-        setWarnignMessage(data.error)
-        notify()
-        console.log(data.error);
-      }
-      else {
-        setToken(data.access_token);
-        setUser(data.user);
-        setIsLogged('true');
-        navigate(`/${data.user.role[0].description}`)
-      }
-      dispatch(login(data.user));
-   }
+    
+    const formData = new FormData(event.currentTarget);
+    const form = {
+      email: formData.get("email"),
+      password: formData.get("password"),
+    };
+    
+    const {data} = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/auth/login`, form);
+    
+    if (data.status === parseInt("401")) {
+      setErrorMessage(data);
+    } else if (data.error) {
+      // comentario de jose
+      setWarnignMessage(data.error)
+      notify()
+      console.log(data.error);
+    } else {
+      setToken(data.access_token);
+      setUser(data.user);
+      setIsLogged('true');
+      navigate(`/${data.user.role[0].description}`)
+    }
+    dispatch(login(data.user));
   };
-
+  
   return (
     <LayoutH>
       <ToastContainer/>
       <div className="flex-grow flex items-center justify-center">
-          <form className="w-72 h-96" onSubmit={handleLogin}>
-            <div className="flex items-center justify-center">
-              <img
-                src={IconHomeLogin}
-                alt="Icono para home"
-                className="h-20 w-20 mb-3"
-              />
-            </div>
-            <h2 className="text-2xl text-center font-bold mb-2">
-              Bienvenido !
-            </h2>
-            <div className="mb-3">
-              <input
-                type="email"
-                id="email"
-                name="email"
-                required
-                autoComplete="email"
-                placeholder="Ingresar correo electronico"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="mt-1 p-2 border rounded w-full
+        <form className="w-72 h-96" onSubmit={handleLogin}>
+          <div className="flex items-center justify-center">
+            <img
+              src={IconHomeLogin}
+              alt="Icono para home"
+              className="h-20 w-20 mb-3"
+            />
+          </div>
+          <h2 className="text-2xl text-center font-bold mb-2">
+            Bienvenido !
+          </h2>
+          <div className="mb-3">
+            <input
+              type="email"
+              id="email"
+              name="email"
+              required
+              autoComplete="email"
+              placeholder="Ingresar correo electronico"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="mt-1 p-2 border rounded w-full
                "
-              />       
-            </div>
-            <div className="relative mb-4">
-              <input
-                type={showPassword ? "text" : "password"}
-                id="password"
-                name="password"
-                required
-                autoComplete="password"
-                placeholder="Ingresar contraseña"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="mt-1 p-2 border rounded w-full"
-              />
-              <button
-                type="button"
-                className="absolute inset-y-0 right-0 flex items-center px-3"
-                onClick={togglePasswordVisibility}
-              >
-                {showPassword ? (
-                  <img
-                    className="h-5 w-5 text-gray-400"
-                    src={IconEye}
-                    alt="ÍconoEyeOn"
-                  />
-                ) : (
-                  <img
-                    className="h-5 w-5 text-gray-400 "
-                    src={IconOffEye}
-                    alt="ÍconoEyeOff"
-                  />
-                )}
-              </button>
-            </div>
-            <div className="text-right mb-2">
-              <Link
-                to="/resetPassword"
-                className="text-[14px]
+            />
+          </div>
+          <div className="relative mb-4">
+            <input
+              type={showPassword ? "text" : "password"}
+              id="password"
+              name="password"
+              required
+              autoComplete="password"
+              placeholder="Ingresar contraseña"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="mt-1 p-2 border rounded w-full"
+            />
+            <button
+              type="button"
+              className="absolute inset-y-0 right-0 flex items-center px-3"
+              onClick={togglePasswordVisibility}
+            >
+              {showPassword ? (
+                <img
+                  className="h-5 w-5 text-gray-400"
+                  src={IconEye}
+                  alt="ÍconoEyeOn"
+                />
+              ) : (
+                <img
+                  className="h-5 w-5 text-gray-400 "
+                  src={IconOffEye}
+                  alt="ÍconoEyeOff"
+                />
+              )}
+            </button>
+          </div>
+          <div className="text-right mb-2">
+            <Link
+              to="/resetPassword"
+              className="text-[14px]
              text-blue-600"
-              >
-                Recurperar contraseña{" "}
-              </Link>
-            </div>
-            <div className="flex items-center justify-between">              
-
-              <ButtonPrimary title={'Registrarse'} icono={IconRegister} typeB='button' to={'/signup'}/>
-
-              
-              <ButtonPrimary title={'Iniciar Sesión'} icono={IconLogin} typeB='submit' to={''}/>
-            </div>
-          </form>        
+            >
+              Recurperar contraseña{" "}
+            </Link>
+          </div>
+          <div className="flex items-center justify-between">
+            
+            <ButtonPrimary title={'Registrarse'} icono={IconRegister} typeB='button' to={'/signup'}/>
+            
+            
+            <ButtonPrimary title={'Iniciar Sesión'} icono={IconLogin} typeB='submit' to={''}/>
+          </div>
+        </form>
       </div>
     </LayoutH>
   );
--- src/pages/Home/SignUp.jsx
+++ src/pages/Home/SignUp.jsx
@@ -1,91 +1,144 @@
-import { useForm } from "react-hook-form";
+import {useForm} from "react-hook-form";
 
 import LayoutH from "../../components/LayoutHome";
 import IconRegister from "../../assets/Img/IconRegister.svg";
 import IconHomeLogin from "../../assets/Img/IconHomeLogin.svg";
 import axios from "axios";
-import { useState, useEffect } from "react";
+import {useState, useEffect} from "react";
 import ButtonPrimary from "../../components/Buttons/primary.jsx";
+import {useLocalStorage} from "../../components/localStorage/index.jsx";
+import {login} from "../../redux/states/authSlice.js";
+import {ToastContainer, toast} from 'react-toastify';
+import {Link, json, useNavigate} from "react-router-dom";
 
-const  SignUpForm = () => {
-  const [options, setOptions]=useState([]);
+const SignUpForm = () => {
+  const [options, setOptions] = useState([]);
+  const [selectedOption, setSelectedOption] = useState(null);
+  const [getDniTypesUL, setDniTypesUL] = useLocalStorage('dniTypes');
+  const [getWarnignMessage, setWarnignMessage] = useLocalStorage('warnignLogin');
+  
+  
   const [email, setEmail] = useState("");
   const [dni, setDni] = useState("");
-  const [dniTypes, setDniTypes] = useState("");
-
+  const [dniType, setDniType] = useState("");
+  
+  let navigate = useNavigate();
+  
+  //se utiliza para dejar inicializado el valor del State de options
+  const jsonData = {
+    data: []
+  };
+  
+  //carga de los dniTypes en el Select
   useEffect(() => {
     const getDniTypes = async () => {
       try {
-        let dniTypes = JSON.parse(localStorage.getItem('dniTypes'));
-        console.log('1')
-        if (!dniTypes) {
-          console.log('2')
+        if (!getDniTypesUL() || options) {
           const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/dni-types`);
-          console.log('3')
-          dniTypes = response.data;
-          console.log('4')
-          localStorage.setItem('dniTypes', JSON.stringify(dniTypes));
-          console.log('5')
+          if (response.data.length > 1) {
+            setDniTypesUL(response.data);
+            setOptions(getDniTypesUL());
+          } else {
+            setOptions(jsonData.data);
+          }
+        } else {
+          setOptions(jsonData.data);
         }
-        console.log('6')
-        setOptions(dniTypes);
-        console.log('7')
       } catch (error) {
         console.error('Error al obtener los datos:', error);
-        localStorage.setItem('dniTypes', JSON.stringify('{id: 0, description: "hola"}'));
+        console.log('Error al obtener los datos:', error);
       }
     };
     getDniTypes();
-},[]);
-
-
-  // funcion register: registrar los difrentes campos
-  // handlesubmit: gestionar el envio de datos, la accion de enviar los datos
-  //
-  const {
-    register,
-    handleSubmit,
-  } = useForm();
-  // Data hace referencia a los datos de el formulario que va a recibir la funcion
-  const onSubmit = (data) => {};
+  }, []);
+  
+  //evento que ejecuta la validacion y activacion del usuario
+  
+  const notifyW = () => {
+    toast.warning(getWarnignMessage());
+  }
+  
+  const notifyS = () => {
+    toast.done(getWarnignMessage());
+  }
+  
+  const handleSignUp = async (event) => {
+    event.preventDefault();
+    
+    const formData = new FormData(event.currentTarget);
+    const form = {
+      email: formData.get("email"),
+      dni: parseInt(formData.get("dni")),
+      dniTypeId: parseInt(formData.get("dniType"))
+    };
+    
+    console.log(form);
+    
+    const {data} = await axios.put(`${import.meta.env.VITE_BACKEND_URL}/users/signup`, form);
+    console.log(data)
+    if (data.status === parseInt("401")) {
+      setErrorMessage(data);
+    } else if (data.error) {
+      setWarnignMessage(data.error)
+      notifyW()
+      console.log(data.error);
+    } else {
+      setWarnignMessage(data.success)
+      notifyS()
+      console.log(data.success)
+      navigate(`/login`)
+    }
+  };
+  
+  //estructura del componente
   return (
     <LayoutH>
+      <ToastContainer/>
       <div className="flex-grow flex items-center justify-center">
-        <form className='w-72 h-96' onSubmit={handleSubmit(onSubmit)}>
+        <form className='w-72 h-96' onSubmit={handleSignUp}>
           <div className="flex items-center justify-center">
             <img
-                src={IconHomeLogin}
-                alt="Icono para home"
-                className="h-20 w-20 mb-3"
+              src={IconHomeLogin}
+              alt="Icono para home"
+              className="h-20 w-20 mb-3"
             />
           </div>
           <h2 className="text-2xl text-center font-bold mb-2">Registro</h2>
           <div className="mb-2 flex flex-col gap-1 items-start">
-            <select className="mt-1 p-2 border rounded w-full" value={dniTypes ? dniTypes : undefined}  onChange={(e) => setDniTypes(e.target.id)}>
-              {JSON.parse(localStorage.getItem('dniTypes')).map(option => (
-                  <option key={option.id} value={option.id}>{option.description}</option>
+            <select className="mt-1 p-2 border rounded w-full"
+                    value={selectedOption ? selectedOption : undefined}
+                    onChange={(e) => setSelectedOption(e.target.description)}
+                    id="dniType"
+                    name="dniType">
+              <option key={0} value={0}>{"Seleccione"}</option>
+              {options.map(option => (
+                <option key={option.id} value={option.id}>{option.description}</option>
               ))}
             </select>
           </div>
           <div className="mb-2 flex flex-col gap-1 items-center">
             <input
-                type="text"
-                placeholder="Ingresar numero de documento"
-                className="mt-1 p-2 border rounded w-full"
-                required={true}
-                onChange={(e) => setDni(e.target.value)}
+              type="number"
+              id="dni"
+              name="dni"
+              placeholder="Ingresar numero de documento"
+              className="mt-1 p-2 border rounded w-full [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+              required={true}
+              onChange={(e) => setDni(e.target.value)}
             />
           </div>
-
+          
           <div className="mb-2 flex flex-col gap-2 items-center">
             <input
-                type="email"
-                placeholder="Ingresar correo institucional"
-                className="mt-1 p-2 border rounded w-full"
-                required={true}
-                onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Ingresar correo institucional"
+              className="mt-1 p-2 border rounded w-full"
+              required={true}
+              onChange={(e) => setEmail(e.target.value)}
             />
-           <ButtonPrimary title={'Registrar'} icono={IconRegister} typeB='submit' to={''}/>
+            <ButtonPrimary title={'Registrar'} icono={IconRegister} typeB='submit' to={''}/>
           </div>
         </form>
       </div>
